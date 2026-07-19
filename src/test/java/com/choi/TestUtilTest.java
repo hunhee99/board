290e87fcf556cef84055eb8222ca68e998ff3493
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-07-19 17:01:01 +0900

    AppTest t1, t2 - green

--- src/test/java/com/choi/TestUtilTest.java
+++ src/test/java/com/choi/TestUtilTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,9 +30,9 @@ public class TestUtilTest {
 
     @Test
     @DisplayName("TestUtil.setOutToByteArray()")
-    void t2() {
+    void t2() throws IOException {
 
-        ByteArrayOutputStream outputStream =  TestUtil.setOutToByteArray();
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
 
         System.out.println("1 / 이순신 / 나의 죽음을 적에게 알리지 마라");
 
@@ -39,7 +40,7 @@ public class TestUtilTest {
 
         TestUtil.clearSetOutToByteArray(outputStream);
 
-        assertThat(outStr).isEqualTo("1 / 이순신 / 나의 죽음을 적에게 알리지 마라");
+        assertThat(outStr).contains("1 / 이순신 / 나의 죽음을 적에게 알리지 마라");
 
         System.out.println("이제 화면에 출력됩니다.");
     }
