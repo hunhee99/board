39e678037b688e5838e581fb94331bf635c006f9
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-07-20 16:10:20 +0900

    t12 -red && 테스트 주석 해제

--- src/test/java/com/choi/AppTest.java
+++ src/test/java/com/choi/AppTest.java
@@ -67,26 +67,26 @@ public class AppTest {
         assertThat(out).contains("2번 명언이 등록되었습니다.");
     }
 
-//    @Test
-//    @DisplayName("목록")
-//    void t5(){
-//        String out = AppTestRunner.run("""
-//                등록
-//                현재를 사랑하라.
-//                작자미상
-//                등록
-//                과거에 집착하지 마라.
-//                작자미상
-//                목록
-//                종료
-//                """);
-//        assertThat(out).contains("""
-//                번호 / 작가 / 명언
-//                ----------------------
-//                2 / 작자미상 / 과거에 집착하지 마라.
-//                1 / 작자미상 / 현재를 사랑하라.
-//                """);
-//    }
+    @Test
+    @DisplayName("목록")
+    void t5(){
+        String out = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                종료
+                """);
+        assertThat(out).contains("""
+                번호 / 작가 / 명언
+                ----------------------
+                2 / 작자미상 / 과거에 집착하지 마라.
+                1 / 작자미상 / 현재를 사랑하라.
+                """);
+    }
 
     @Test
     @DisplayName("명언삭제")
@@ -125,61 +125,61 @@ public class AppTest {
         assertThat(out).contains("1번 명언은 존재하지 않습니다.");
     }
 
-//    @Test
-//    @DisplayName("명언수정")
-//    void t8(){
-//        String out = AppTestRunner.run("""
-//                등록
-//                현재를 사랑하라.
-//                작자미상
-//                등록
-//                과거에 집착하지 마라.
-//                작자미상
-//                목록
-//                삭제?id=1
-//                삭제?id=1
-//                수정?id=3
-//                수정?id=2
-//                현재와 자신을 사랑하라.
-//                홍길동
-//                목록
-//                종료
-//                """);
-//
-//        assertThat(out).contains("3번 명언은 존재하지 않습니다.");
-//        assertThat(out).contains("""
-//                번호 / 작가 / 명언
-//                ----------------------
-//                2 / 홍길동 / 현재와 자신을 사랑하라.
-//                """);
-//    }
+    @Test
+    @DisplayName("명언수정")
+    void t8(){
+        String out = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                삭제?id=1
+                삭제?id=1
+                수정?id=3
+                수정?id=2
+                현재와 자신을 사랑하라.
+                홍길동
+                목록
+                종료
+                """);
 
-//    @Test
-//    @DisplayName("파일을 통한 영속성")
-//    void t9(){
-//        String out1 = AppTestRunner.run("""
-//                등록
-//                현재를 사랑하라.
-//                작자미상
-//                등록
-//                과거에 집착하지 마라.
-//                작자미상
-//                목록
-//                종료
-//                """);
-//        String out2 = AppTestRunner.run("""
-//                목록
-//                종료
-//                """);
-//
-//        assertThat(out2).contains("""
-//                번호 / 작가 / 명언
-//                ----------------------
-//                2 / 작자미상 / 과거에 집착하지 마라.
-//                1 / 작자미상 / 현재를 사랑하라.
-//                """);
-//
-//    }
+        assertThat(out).contains("3번 명언은 존재하지 않습니다.");
+        assertThat(out).contains("""
+                번호 / 작가 / 명언
+                ----------------------
+                2 / 홍길동 / 현재와 자신을 사랑하라.
+                """);
+    }
+
+    @Test
+    @DisplayName("파일을 통한 영속성")
+    void t9(){
+        String out1 = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                종료
+                """);
+        String out2 = AppTestRunner.run("""
+                목록
+                종료
+                """);
+
+        assertThat(out2).contains("""
+                번호 / 작가 / 명언
+                ----------------------
+                2 / 작자미상 / 과거에 집착하지 마라.
+                1 / 작자미상 / 현재를 사랑하라.
+                """);
+
+    }
 
     @Test
     @DisplayName("data.json 빌드")
@@ -252,5 +252,44 @@ public class AppTest {
                 2 / 작자미상 / 과거에 집착하지 마라.
                 1 / 작자미상 / 현재를 사랑하라.
                 """);
+    }
+
+    @Test
+    @DisplayName("페이징")
+    void t12(){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            sb.append("등록\n&d\n%d\n".formatted(i));
+        }
+        sb.append("목록\n");
+        sb.append("목록?page=2\n");
+        sb.append("종료\n");
+
+        String out = AppTestRunner.run(sb.toString());
+
+        assertThat(out).contains("""
+                번호 / 작가 / 명언
+                ----------------------
+                10 / 작자미상 10 / 명언 10
+                9 / 작자미상 9 / 명언 9
+                8 / 작자미상 8 / 명언 8
+                7 / 작자미상 7 / 명언 7
+                6 / 작자미상 6 / 명언 6
+                ----------------------
+                페이지 : [1] / 2
+                """);
+
+        assertThat(out).contains("""
+                번호 / 작가 / 명언
+                ----------------------
+                5 / 작자미상 5 / 명언 5
+                4 / 작자미상 4 / 명언 4
+                3 / 작자미상 3 / 명언 3
+                2 / 작자미상 2 / 명언 2
+                1 / 작자미상 1 / 명언 1
+                ----------------------
+                페이지 : 1 / [2]
+                """);
+
     }
 }
